70d96b91441f32e297b75c5134c438539cfeb3bf
ilmar <khoseilmar@gmail.com>
2021-07-09 15:06:06 -0300

    refatoração

--- src/principal/Caso1.java
+++ src/principal/Caso1.java
@@ -8,31 +8,13 @@ import aima.core.search.csp.Variable;
 import aima.core.search.csp.examples.NotEqualConstraint;
 
 public class Caso1 extends Caso {
-	public static final Variable COMP0408_dia1 = new Variable("COMP0408_dia1");
-	public static final Variable COMP0408_dia2 = new Variable("COMP0408_dia2");
-	public static final Variable COMP0455_dia1 = new Variable("COMP0455_dia1");
-	public static final Variable COMP0455_dia2 = new Variable("COMP0455_dia2");
-	public static final Variable COMP0481 = new Variable("COMP0481");
-	
 	@Override
 	void addDisciplinas() {
-		addVariable(COMP0408_dia1);
-		addVariable(COMP0408_dia2);
-		addVariable(COMP0455_dia2);
-		addVariable(COMP0455_dia1);
-		addVariable(COMP0481);
-		
-		Domain<Horario> dominio_COMP0408_dia1 = new Domain<>(new Horario(SEGUNDA, 21, 2));
-		Domain<Horario> dominio_COMP0408_dia2 = new Domain<>(new Horario(QUARTA, 21, 2));
-		Domain<Horario> dominio_COMP0455_dia1 = new Domain<>(new Horario(TERCA, 15, 2));
-		Domain<Horario> dominio_COMP0455_dia2 = new Domain<>(new Horario(QUINTA, 15, 2));
-		Domain<Horario> dominio_COMP0481 = new Domain<>(new Horario(QUINTA, 19, 2));
-		
-		setDomain(COMP0408_dia1, dominio_COMP0408_dia1);
-		setDomain(COMP0408_dia2, dominio_COMP0408_dia2);
-		setDomain(COMP0455_dia1, dominio_COMP0455_dia1);
-		setDomain(COMP0455_dia2, dominio_COMP0455_dia2);
-		setDomain(COMP0481, dominio_COMP0481);
+		addDisciplina("COMP0408_dia1", SEGUNDA, 21, 2);
+		addDisciplina("COMP0408_dia2", QUARTA, 21, 2);
+		addDisciplina("COMP0455_dia1", TERCA, 15, 2);
+		addDisciplina("COMP0455_dia2", QUINTA, 15, 2);
+		addDisciplina("COMP0481", QUINTA, 19, 2);
 	}
 	
 	public Caso1() {
--- src/principal/Projeto.java
+++ src/principal/Projeto.java
@@ -11,6 +11,19 @@ import aima.core.search.csp.solver.CspSolver;
 import aima.core.search.csp.solver.FlexibleBacktrackingSolver;
 
 public class Projeto {
+	public static void linha_barra(int largura) {
+		// horário
+		for (int i = 0; i < 13; i++)
+			System.out.print("_");
+		System.out.print("_|_");
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < largura; j++)
+				System.out.print("_");
+			System.out.print("_|_");
+		}
+		System.out.println("");
+	}
+	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
@@ -26,6 +39,19 @@ public class Projeto {
 			return;
 		}
 		
+		System.out.println("Para adicionar uma tarefa diária, digite o nome, o horário de início e a duração da tarefa.");
+		System.out.println("Para parar de adicionar tarefas diárias, digite apenas 0");
+		while (true) {
+			String nome = in.next();
+			if (nome.equals("0"))
+				break;
+			
+			int inicio = in.nextInt();
+			int duracao = in.nextInt();
+			
+			caso.addTarefaDiaria(nome, inicio, duracao);
+		}
+		
 		System.out.println("Para adicionar uma tarefa, digite o nome e a duração da tarefa.");
 		System.out.println("Para parar de adicionar tarefas, digite apenas 0");
 		while (true) {
@@ -38,17 +64,58 @@ public class Projeto {
 			caso.addTarefa(nome, duracao);
 		}
 		
+		String[][] tabela = new String[48][6];
+		int largura = 3;
+		
 		CspSolver<Variable, Horario> algoritmo = new FlexibleBacktrackingSolver<Variable, Horario>().setAll();
 		Optional<Assignment<Variable, Horario>> solucao = algoritmo.solve(caso);
 		if (solucao.isPresent()) {
 			for (Variable var : solucao.get().getVariables()) {
 				Horario horario = solucao.get().getValue(var);
-				System.out.println(var.getName());
-				System.out.println("Dia: " + horario.dia);
-				System.out.println("Hora: " + horario.inicio);
-				System.out.println("Duração: " + horario.duracao);
-				System.out.println("");
+//				System.out.println(var.getName());
+//				System.out.println("Dia: " + horario.dia);
+//				System.out.println("Hora: " + horario.inicio);
+//				System.out.println("Duração: " + horario.duracao);
+//				System.out.println("");
+				
+				largura = Math.max(largura, var.getName().length());
+				for (int i = horario.inicio * 2; i < horario.inicio * 2 + (horario.duracao * 2); i++) {
+					System.out.println((i % 48) + " " + (horario.dia + (i / 48)));
+					tabela[i % 48][horario.dia + (i / 48)] = var.getName(); 
+				}
 			}
+		}
+		
+		for (int hora = 0; hora < 48; hora++) {
+			//linha_barra(largura);
+			
+			String add_0_primeiro = "";
+			String add_0_segundo = "";
+			if (hora / 2 < 10) {
+				add_0_primeiro = "0";
+				add_0_segundo = "0";
+			}
+			if (hora % 2 == 1 && hora / 2 == 9) {
+				add_0_segundo = "";
+			}
+			
+			if (hora % 2 == 0)
+				System.out.print(add_0_primeiro + (hora / 2) + ":00 - " + add_0_segundo + (hora / 2) + ":30");	
+			else
+				System.out.print(add_0_primeiro + (hora / 2) + ":30 - " + add_0_segundo + (hora / 2 + 1) + ":00");
+			
+			System.out.print(" | ");
+			for (int dia = 0; dia < 6; dia++) {
+				int falta = largura;
+				if (tabela[hora][dia] != null) {
+					System.out.print(tabela[hora][dia]);
+					falta -= tabela[hora][dia].length();
+				}
+				for (int c = 0; c < falta; c++)
+					System.out.print(" ");
+				System.out.print(" | ");
+			}
+			System.out.println("");
 		}
 	}
 }
